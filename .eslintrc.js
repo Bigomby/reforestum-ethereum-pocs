d8fdd4a4962f17e84551320e6b4d2f0196f1bb69
Diego Fernández Barrera <bigomby@gmail.com>
2018-10-16 12:39:11 +0200

    :wrench: Update linters config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,34 @@
 module.exports = {
   root: true,
+
+  parser: "vue-eslint-parser",
+
+  parserOptions: {
+    parser: "babel-eslint",
+    sourceType: "module"
+  },
+
   env: {
     browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {}
-}
+
+  extends: ["eslint:recommended", "plugin:vue/recommended"],
+
+  rules: {
+    "vue/html-closing-bracket-newline": ["error", { multiline: "always" }],
+
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "always-multiline"
+      }
+    ],
+    quotes: ["error", "single"],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }]
+  }
+};

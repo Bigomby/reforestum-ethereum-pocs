88628dd9e193ad200383c28fa3f20620d85eb478
Diego Fernández Barrera <bigomby@gmail.com>
2018-11-21 11:25:39 +0100

    :wrench: Improve nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,44 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+  mode: 'spa',
+
+  env: {
+    RFT_CONTRACT_ADDRESS: process.env.RFT_CONTRACT_ADDRESS,
+  },
+
   head: {
-    title: 'reforestum-ethereum',
+    title: 'Reforestum',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      {
+        rel: 'stylesheet',
+        href: 'http://fonts.googleapis.com/icon?family=Material+Icons',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+    ],
   },
-  /*
-  ** Customize the progress bar color
-  */
+
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
+
+  srcDir: 'client/',
+
   build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+          exclude: /(node_modules)/,
+        });
       }
-    }
-  }
-}
+    },
+  },
 
+  modules: ['nuxt-buefy'],
+
+  plugins: [{ src: '~/plugins/rft', ssr: false }],
+};
